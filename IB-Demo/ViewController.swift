1250b1beb9e9da363413ef118863c2f0c6add0f5
Simon <simimueller@hotmail.com>
2020-09-26 13:46:06 +0200

    finish exercise 2

--- IB-Demo/ViewController.swift
+++ IB-Demo/ViewController.swift
@@ -13,14 +13,21 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         slider.setValue(50.0, animated: false)
         sliderLabel.text = String(50.0)
+        button.setTitle("Start spinning", for: .normal)
     }
 
     @IBOutlet weak var sliderLabel: UILabel!
     @IBOutlet weak var spinner: UIActivityIndicatorView!
     @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var button: UIButton!
     
     @IBAction func sliderValueChanged(_ sender: UISlider) {
         sliderLabel.text = String(sender.value.rounded())
+        if sender.value.rounded() == 70.0 {
+            let alert = UIAlertController(title: "Important", message: "You are now in the slider's danger zone!", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true)
+        }
     }
     
     @IBAction func colorButtonValueChanged(_ sender: UISegmentedControl) {
@@ -34,19 +41,25 @@ class ViewController: UIViewController {
     @IBAction func spinButtonPressed(_ sender: UIButton) {
         if spinner.isAnimating {
             spinner.stopAnimating()
-//            sender.setTitle("Stop spinning", for: .normal)
+            sender.setTitle("Start spinning", for: .normal)
         } else {
             spinner.startAnimating()
-//            sender.setTitle("Start spinning", for: .normal)
+        
+            sender.setTitle("Stop spinning", for: .normal)
         }
     }
     
     
-//    @IBAction func infoButtonPressed(_ sender: UIButton) {
-//        let alert = UIAlertController(title: "My Alert", message: "This is an alert.", preferredStyle: .alert)
-//        alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: { _ in
-//        NSLog("The \"OK\" alert occured.")
-//        }))
-//    }
+    @IBAction func infoButtonPressed(_ sender: UIButton) {
+        let alert = UIAlertController(title: "Today's info", message: "One sponsor of HSLU Informatik is Glencore, which causes worldwide environmental disasters and human rights violations.", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+        alert.addAction(UIAlertAction(title: "...really?", style: .default, handler: alertHandler ))
+        self.present(alert, animated: true)
+    }
+    
+    func alertHandler(alert: UIAlertAction!) {
+        guard let url = URL(string: "https://konzern-initiative.ch/skandal/mine-vergiftet-kinder/") else { return }
+        UIApplication.shared.open(url)
+    }
 }
 
